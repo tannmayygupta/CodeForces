0709f861ec6ca6085c196136a794ff75905c507e
tannmayygupta <guptatb_1@rknec.edu>
2025-03-21 14:58:46 +0530

    A. Way Too Long Words(hard75 ques done)

--- WayTooLongWordsD1.java
+++ WayTooLongWordsD1.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class WayTooLongWordsD1{
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        String[] words = new String[n];
+        for(int i = 0 ; i < n ; i++){
+            words[i] = sc.nextLine();
+        }
+
+        for(int i = 0 ; i < n ; i++){
+            // System.out.println(words[i]);
+
+            if(words[i].length() > 10){
+                char frst = words[i].charAt(0);
+                char last = words[i].charAt(words[i].length()- 1 );
+                int j = words[i].length() - 2 ;
+                System.out.println(frst+""+j+""+last);
+            }
+            else{
+                System.out.println(words[i]);
+            }
+        }
     }
 }
