4bcc2d077942afe635d06b3cae133503ee77ad96
tannmayygupta <guptatb_1@rknec.edu>
2025-03-24 16:35:03 +0530

    rename the file waytoolongword simililar to other files

--- D1WayTooLongWords.java
+++ D1WayTooLongWords.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class WayTooLongWordsD1{
+public class D1WayTooLongWords{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
