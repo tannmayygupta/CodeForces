e491398d97964f7801e42faeb6dca1aadda0a41e
tannmayygupta <guptatb_1@rknec.edu>
2025-03-31 17:53:08 +0530

    A. Next Round (hard75 ques done DAY 10)

---
+++ D10NextRound.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class D10NextRound{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int count = 0 ;
+
+        int m = arr[k -1];
+
+        for(int i = 0 ; i < n ; i++){
+            if(arr[i] >= m && arr[i] > 0){
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
