04b0f1751c42b94ac4760407d545fb5b1db530a2
tannmayygupta <tanmayg460@gmail.com>
2025-08-16 11:27:49 +0530

    https://codeforces.com/contest/734/problem/A

---
+++ CP31/800/AntonandDanik.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+// A. Anton and Danik
+// https://codeforces.com/contest/734/problem/A
+public class AntonandDanik {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt() ;
+
+        String s = sc.next();
+
+        int Anton = 0 ;
+
+        int Danik = 0 ;
+
+
+        for(int i = 0 ; i < n ; i++){
+            if(s.charAt(i) == 'A'){
+                Anton++;
+            }
+            else{
+                Danik++;
+            }
+        }
+
+        if(Anton == Danik){
+            System.out.println("Friendship");
+        }
+        else if(Anton > Danik){
+            System.out.println("Anton");
+        }
+        else{
+            System.out.println("Danik");
+        }
+    }
+}
