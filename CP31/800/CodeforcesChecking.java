b50831c980ff81932053d17776f727096b0979ad
tannmayygupta <tanmayg460@gmail.com>
2025-08-19 00:27:53 +0530

    Codeforces ques for my rating hard75 letsgoo(A. Codeforces Checking)

---
+++ CP31/800/CodeforcesChecking.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class CodeforcesChecking {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while(t!=0){
+            boolean[] cf = new boolean[26];
+            // codeforces
+            cf[2] = true ; // c
+            cf[14] = true ; // o
+            cf[3] = true ; // d
+            cf[4] = true ; // e
+            cf[5] = true ; // f
+            cf[17] = true ; // r
+            cf[18] = true ; // s
+            
+            char ch = sc.next().charAt(0);
+            int c = ch - 'a';
+            if(cf[c]){
+                System.out.println("Yes");
+            }else{
+                System.out.println("No");
+            }
+            
+            t--;
+        }
+    }
+}
