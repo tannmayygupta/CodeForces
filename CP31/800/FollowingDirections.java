8b9e902020425e6ae553959d58e5a1ecfdba8983
tannmayygupta <tanmayg460@gmail.com>
2025-08-18 19:54:23 +0530

    Codeforces ques for my rating hard75 letsgoo(B. Following Directions)

---
+++ CP31/800/FollowingDirections.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class FollowingDirections {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t != 0) {
+
+            int N = sc.nextInt();
+
+            String s = sc.next();
+
+            int x = 0 ;
+
+            int y = 0 ;
+
+            int flag = 0 ;
+
+            for(int i = 0 ; i < N ; i++){
+                char ch = s.charAt(i);
+
+                if(ch == 'R'){
+                    x = x + 1 ;
+                    if(check(x, y)){
+                        flag = 1 ;
+                        break;
+                    }
+                }
+                else if(ch == 'L'){
+                    x = x - 1 ;
+                    if(check(x, y)){
+                        flag = 1 ;
+                        break;
+                    }
+                }
+                else if(ch == 'U'){
+                    y = y + 1 ;
+                    if(check(x, y)){
+                        flag = 1 ;
+                        break;
+                    }
+                }
+                else if(ch == 'D'){
+                    y = y - 1 ;
+                    if(check(x, y)){
+                        flag = 1 ;
+                        break;
+                    }
+                }
+            }
+
+            if(flag == 1){
+                System.out.println("Yes");
+            }
+            else{
+                System.out.println("No");
+            }
+            
+            t--;
+        }
+
+    }
+
+    static boolean check(int x , int y){
+        
+        if(x == 1 && y == 1){
+            return true ;
+        }
+        
+        return false ;
+    }
+}
