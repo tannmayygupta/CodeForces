12b7536735465d2dd004b26806964cac26b3b72d
tannmayygupta <tanmayg460@gmail.com>
2025-08-22 23:22:53 +0530

    Codeforces ques for my rating hard75 letsgoo(A. YES or YES?)

---
+++ CP31/800/YESorYES.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+// A. YES or YES?
+// https://codeforces.com/contest/1703/problem/A
+public class YESorYES {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t!=0) {
+            String s = sc.next().toLowerCase();
+
+            if(s.equals("yes")){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+            
+            t--;
+        }
+    }
+}
