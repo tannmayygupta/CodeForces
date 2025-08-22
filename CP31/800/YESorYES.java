9347206879a11ca40125f06ce95f9a153f4b63f9
tannmayygupta <tanmayg460@gmail.com>
2025-08-22 23:23:30 +0530

    Codeforces ques for my rating hard75 letsgoo(A. YES or YES?)

--- CP31/800/YESorYES.java
+++ CP31/800/YESorYES.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 // A. YES or YES?
 // https://codeforces.com/contest/1703/problem/A
+
+
 public class YESorYES {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
