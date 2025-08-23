0198825926ba2cad3bc61e447b8f38b72c4781a9
tannmayygupta <tanmayg460@gmail.com>
2025-08-23 19:07:25 +0530

    Codeforces ques for my rating hard75 letsgoo(C. Different Differences)

--- CP31/800/DiffrentDiffrence.java
+++ CP31/800/DiffrentDiffrence.java
@@ -8,6 +8,10 @@ public class DiffrentDiffrence {
         int t = sc.nextInt();
 
         while(t!=0){
+
+            // eg: x, x+1, x+3, x+6, x+10
+            // lets take x = 1 
+            //     1, 2, 3,
             int k = sc.nextInt();
 
             int n = sc.nextInt();
@@ -17,7 +21,10 @@ public class DiffrentDiffrence {
             System.out.print(ans+" ");
 
             for(int i = 1 ; i <= k - 1 ; i++){
-                if(n - ans - i  + 1 < k - i){
+                if(n - ans - i  + 1 < k - i){ // check ki the array we are creating is of duplicate elemt and should be in range 
+                                              // it should be of max characteristic 
+
+                    // for in case 
                     ans++;
                 }else{
                     ans += i ;
