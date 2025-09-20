2c4474a7267ea5d2ad8b59e461ca1438e65ca762
tannmayygupta <tanmayg460@gmail.com>
2025-09-20 17:31:41 +0530

    Codeforces que for  my rating by ACD(A. In Search of an Easy Problem)

---
+++ CP31/800/InSearchofanEasyProblem.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class InSearchofanEasyProblem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt() ;
+
+        int[] arr = new int[n];
+
+        int cou = 0 ;
+
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = sc.nextInt();
+            if(arr[i] == 1){
+                cou++;
+            }
+        }
+
+        if(cou >= 1){
+            System.out.println("HARD");
+        }else{
+            System.out.println("EASY");
+        }
+    }
+}
