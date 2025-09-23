eda119b6769dd3a81444f076185db86268e94608
tannmayygupta <tanmayg460@gmail.com>
2025-09-23 22:39:08 +0530

    Codeforces que in myrating by ACD Bot(A. Anton and Letters)

---
+++ CP31/800/AntonandLetters.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class AntonandLetters {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        int n = s.length();
+
+        int count = 0 ;
+
+        int[] freq = new int[26];
+
+        for(int i = 0 ; i < n ; i++){
+            char ch = s.charAt(i);
+
+            int c = ch -'a' ;
+
+            if(c >= 0 && c <= 25){
+                freq[c]++;
+            }
+        }
+
+        for(int i = 0 ; i < 26 ; i++){
+            if(freq[i] > 0){
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
