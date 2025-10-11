c2f1f7a075527c2d3536feb2eea2a951ccbce2b8
tannmayygupta <tanmayg460@gmail.com>
2025-10-11 16:04:13 +0530

    Codeforces que in my rating by ACD (A. Evenodds)

---
+++ CP31/800/EvenOdds.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class EvenOdds {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        long k = sc.nextLong();
+
+        long m = (n + 1) / 2;
+
+        if (k <= m) {
+            long ans = 2 * (k - 1) + 1;
+            System.out.println(ans);
+        } else {
+            k = k - m;
+            long ans = 2 * (k - 1) + 2;
+            System.out.println(ans);
+        }
+    }
+}
