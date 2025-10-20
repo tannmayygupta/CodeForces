a1e2b569279a5a17e262854a02cf7403340a9c17
tannmayygupta <tanmayg460@gmail.com>
2025-10-20 17:49:49 +0530

    Codeforces que by TLE cp 31 sheet(A. Target Practice)

---
+++ CP31/800/TargetPractice.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class TargetPractice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while(t-- != 0){
+            
+            int[][] ring = {{1,1,1,1,1,1,1,1,1,1},
+                            {1,2,2,2,2,2,2,2,2,1},
+                            {1,2,3,3,3,3,3,3,2,1},
+                            {1,2,3,4,4,4,4,3,2,1},
+                            {1,2,3,4,5,5,4,3,2,1},
+                            {1,2,3,4,5,5,4,3,2,1},
+                            {1,2,3,4,4,4,4,3,2,1},
+                            {1,2,3,3,3,3,3,3,2,1},
+                            {1,2,2,2,2,2,2,2,2,1},
+                            {1,1,1,1,1,1,1,1,1,1} 
+                        };
+
+            char[][] quering = new char[10][10];
+
+            int ans = 0 ;
+
+            for(int i = 0 ; i < 10 ; i++){
+                String row = sc.next();
+                for(int j = 0 ; j < 10 ; j++){
+                    quering[i][j] = row.charAt(j);
+
+                    if(quering[i][j] == 'X'){
+                        ans += ring[i][j];
+                    }
+                }
+            }
+
+            System.out.println(ans);
+
+        }
+    }
+}
