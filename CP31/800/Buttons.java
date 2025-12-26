08d86fb99f704fe7221b9d2e74079f903aa25714
tannmayygupta <tanmayg460@gmail.com>
2025-12-26 22:25:09 +0530

    Buttons under 800 rated cf

--- CP31/800/Buttons.java
+++ CP31/800/Buttons.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 // https://codeforces.com/problemset/problem/1858/A
 // Buttons
+
 public class Buttons {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
