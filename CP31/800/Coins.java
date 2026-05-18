23fa98596ee280c0018bd00557d13599af6d8f4d
tannmayygupta <tanmayg460@gmail.com>
2026-05-18 21:16:55 +0530

    tle 31 days challenge starts today lets go day1 A. Coins

---
+++ CP31/800/Coins.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Coins {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- != 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            // 2*x = n - k*y 
+            // lhs = rhs
+            if(n % 2 == 0 || (n - k) % 2 == 0){
+                // if n is even then its easy to get rhs as even 
+                // second if n is odd, then dependency goes to k if it is odd or even 
+                // if 2nd term even then not possible so n - k becomes even then we say that x and y can exists
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+        }
+    }
+}
