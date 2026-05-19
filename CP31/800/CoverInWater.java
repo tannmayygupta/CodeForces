05d0b86f6a9cdbda17309c729cf4c12b2134468c
tannmayygupta <tanmayg460@gmail.com>
2026-05-19 21:51:47 +0530

    tle 31 days challenge starts today lets go day2 A. Cover in Water

--- CP31/800/CoverInWater.java
+++ CP31/800/CoverInWater.java
@@ -5,22 +5,30 @@ public class CoverInWater {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
 
-        while(t!=0){
+        while(t--!=0){
             int n = sc.nextInt();
-
-            sc.nextLine();
-
-            String s = sc.nextLine();
-
-            int count = 0 ;
-
+            String s = sc.next();
+            int contiEmpty = 0 ; 
+            int totalEmpty = 0 ;
+            int max = 0 ;
             for(int i = 0 ; i < n ; i++){
-                
+                char ch = s.charAt(i);
+                if(ch == '.'){
+                    totalEmpty++;
+                    contiEmpty++;
+                    if(max < contiEmpty){
+                        max = contiEmpty ;
+                    }
+                }else{
+                    contiEmpty = 0 ;
+                }
             }
 
-            System.out.println(count);
-
-            t--;
+            if(max >= 3){
+                System.out.println(2);
+            }else{
+                System.out.println(totalEmpty);
+            }
         }
     }
 }
