b44726a3aa8cf870b2c6d93800fa9cae4fe41512
tannmayygupta <tanmayg460@gmail.com>
2026-05-23 01:14:47 +0530

    lets go day5 of 31days A. Walking Master

---
+++ CP31/800/WalkingMaster.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class WalkingMaster {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t--!=0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            int d = sc.nextInt();
+            int dai = 0 ;
+            int ans = 0 ;
+            int flag = 0 ;
+
+            if(b <= d){
+                dai = d - b;
+                ans += dai ;
+            }else{
+                flag = 1 ;
+            }
+
+            a = a + dai ;
+            b = b + dai ;
+            int left = 0 ;
+            if(a >= c){
+                left = a - c ;
+                ans+= left ;
+            }else{
+                flag = 1 ;
+            }
+
+            if(flag == 1){
+                System.out.println(-1);
+            }else{
+                System.out.println(ans);
+            }
+
+            
+        }
+    }
+}
