90d4f2327d1a1e87a72c4d688229bf343c941b03
tannmayygupta <tanmayg460@gmail.com>
2026-05-23 23:45:26 +0530

    lets go day5 of 31days A. We Need the Zero

---
+++ CP31/800/WeNeedtheZero.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class WeNeedtheZero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt() ;
+
+        while (t--!=0) {
+            int n = sc.nextInt() ;
+            int[] arr = new int[n];
+            for(int i = 0 ; i < n ; i++){
+                arr[i] = sc.nextInt() ;
+            }
+
+            int xor = 0 ;
+            for(int i = 0 ; i < n ; i++){
+                xor = xor ^ arr[i];
+            }
+
+            if(n % 2 == 0){
+                if(xor == 0){
+                    System.out.println(1);
+                }else{
+                    System.out.println(-1);
+                }
+            }else{
+                System.out.println(xor);
+            }
+        }
+    }
+}
