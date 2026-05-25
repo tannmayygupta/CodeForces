a5a06f1c49ea96e88a46d95bc4dcf4ec87633ff9
tannmayygupta <tanmayg460@gmail.com>
2026-05-26 00:41:09 +0530

    lets go day8 of 31days A. Serval and Mocha's Array

---
+++ CP31/800/ServalandMochaArray.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class ServalandMochaArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t--!=0) {
+            int n = sc.nextInt();
+            int[] arr= new int[n];
+            for(int i = 0 ; i < n ; i++){
+                arr[i] = sc.nextInt();
+            }
+            
+            int flag = 0 ;
+
+            for(int i = 0 ; i < n ; i++){
+                for(int j = i + 1 ; j < n ; j++){
+                    if(gcd(arr[i],arr[j]) <= 2){
+                        flag = 1 ;
+                        break ;
+                    }
+                }
+            }
+
+            if(flag == 1){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+        }
+    }
+    public static int gcd(int a, int b){
+        if(b == 0){
+            return a ;
+        }
+        return gcd(b, a%b);
+    }
+}
