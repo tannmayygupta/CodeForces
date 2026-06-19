b734936cced8e8d3cd7ad0564fe9f87369ec5fd7
tannmayygupta <tanmayg460@gmail.com>
2026-06-19 15:15:54 +0530

    completed by cp-31 800 rated question today letsgoo

---
+++ CP31/800/TwoPermutations.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TwoPermutations {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- != 0) {
+            int n = sc.nextInt();
+            int a = sc.nextInt() ;
+            int b = sc.nextInt() ;
+            
+            int left = (n - (a + b)) ;
+
+            if((a == b && b == n) || left >= 2){
+                System.out.println("Yes");
+            }else{
+                System.out.println("No");
+            }
+        }
+    }
+}
